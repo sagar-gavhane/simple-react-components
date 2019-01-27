026b4cbf25b905df1e6aa6dbf8ee208fd32b7168
Sagar <sgavhane70@gmail.com>
2019-01-27 13:13:16 +0530

    App component removed

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-ReactDOM.render(<div />, document.getElementById('root'))
+ReactDOM.render(
+<div />, 
+document.getElementById('root')
+)
